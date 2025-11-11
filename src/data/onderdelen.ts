be7ccd17c4680e83e2e19fe5a69c0afab3c1428c
Noamdev-Noamdev <noam1216@hotmail.com>
2025-11-11 22:42:14 +0100

    Oefeningen chemie H4 toegevoegd

--- src/data/onderdelen.ts
+++ src/data/onderdelen.ts
@@ -117,6 +117,7 @@ export const onderdelenConfig: OnderdelenConfig = {
     { id: "ionenreacties", name: "Ionenreacties", description: "Reacties waarbij ionen worden uitgewisseld", jaar: 6 },
     { id: "redoxreacties", name: "Redoxreacties", description: "Oxidatie- en reductiereacties", jaar: 6 },
     { id: "zuur-base-reacties", name: "Zuur-base-reacties", description: "Reacties tussen zuren en basen", jaar: 6 },
+    { id: "nanomaterialen", name: "Nanomaterialen", description: "Materialen op nanoschaal en hun toepassingen", jaar: 6 },
   ]
 };
  
